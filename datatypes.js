99cd2c76109d7651748c993a7673df79727c98fb
Enkhsolongo <rrainbow1018@gmail.com>
2024-11-25 14:41:46 +0800

    intro

---
+++ datatypes.js
@@ -0,0 +1,53 @@
+// 5. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн 
+// утгуудыг шалгах, number, string, boolean, undefined,  null гэх мэт
+let address = "New York";
+console.log(address, typeof address);
+
+let grade = 100;
+console.log(grade, typeof grade);
+
+const isMarried = false;
+console.log(isMarried, typeof isMarried);
+
+let fullName = null;
+console.log(fullName, typeof fullName);
+
+let wifeName;
+console.log(wifeName, typeof wifeName);
+
+// 6. Утга оноогоогүй хувьсагч зарлах
+let nameBig;
+console.log(nameBig);
+
+// 7. Утга оноосон хувьсагч зарлах
+const teacherName = "Nandir";
+console.log(teacherName);
+
+// 8. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+let firstName = "Olivia";
+console.log(firstName);
+
+lastName = "Rodrigo";
+console.log(lastName);
+
+matiralStatus = true;
+console.log(matiralStatus);
+
+country = "Mongolia";
+console.log(country);
+
+ageNow = 18;
+console.log(ageNow);
+
+numberOne = null;
+console.log(numberOne);
+
+// 9. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+const firstName1 = "Bella";
+lastName = "Hadid";
+matiralStatus = false;
+country = "American";
+ageNow = 24;
+numberOne = null;
+console.log(firstName1, lastName, matiralStatus, country, ageNow, numberOne);
+//HOW i do it?? authored names!!!!
---
+++ variable.js
@@ -0,0 +1,18 @@
+//TASK 
+//"comments can make code readable."
+//"Javascript lesson begin!"
+// Comments can make code readable. 
+let firstName = "John";
+console.log(firstName);
+
+const age = 14;
+console.log(age);
+
+const isAlive = true;
+console.log(isAlive);
+
+const food = null;
+console.log(food);
+
+let surName;
+console.log(surName); //undefined-iig only let dr ughimu?? just next time we can change it.
